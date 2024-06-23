f7aa41fbda49be2822219d766ae4f14a397d44ef
le0nAg <leonardoagostini123@gmail.com>
2024-06-23 16:51:26 +0200

    sso web2 (google/github)

--- src/server.js
+++ src/server.js
@@ -14,9 +14,28 @@ app.get('/api', (req, res) => {
     res.send('Hello from the backend!');
 });
 // Serve static index.html for all other routes
-app.get('*', (req, res) => {
-    res.sendFile(path_1.default.join(__dirname, '/frontend', 'index.html'));
-});
+/*app.get('*', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '/frontend', 'index.html'));
+});*/
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
+//--------------------------------------------
+require("express-openid-connect");
+const { auth } = require('express-openid-connect');
+require('dotenv').config();
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.AUTH0_CLIENT_SECRET,
+    baseURL: 'http://127.0.0.1:8080',
+    clientID: process.env.AUTH0_CLIENT_ID,
+    issuerBaseURL: 'https://dev-rydcu5ktr7kdkapy.us.auth0.com'
+};
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+    console.log(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
--- src/server.ts
+++ src/server.ts
@@ -13,10 +13,35 @@ app.get('/api', (req: Request, res: Response) => {
 });
 
 // Serve static index.html for all other routes
-app.get('*', (req: Request, res: Response) => {
+/*app.get('*', (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, '/frontend', 'index.html'));
-});
+});*/
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
+
+//--------------------------------------------
+import 'express-openid-connect';
+const { auth } = require('express-openid-connect');
+
+require('dotenv').config();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_CLIENT_SECRET,
+  baseURL: 'http://127.0.0.1:8080',
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: 'https://dev-rydcu5ktr7kdkapy.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+    console.log(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
